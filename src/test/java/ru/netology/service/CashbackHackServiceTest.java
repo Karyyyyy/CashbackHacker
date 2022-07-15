5fc4b7490299484a0a2d006e557b41af8eb5351b
Карина Миронова <Twiksyyy@gmail.com>
2022-07-15 17:42:00 +0300

    Branch testng added

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,3 +1,59 @@
 package ru.netology.service;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
 public class CashbackHackServiceTest {
+
+    @Test
+    public void testRemain() {
+        CashbackHackService service = new CashbackHackService();
+        int expected = 1000;
+        int amount = 1000;
+        int result = service.remain(amount);
+
+        assertEquals(result, expected);
+
+    }
+    @Test
+    public void testRemain2() {
+        CashbackHackService service = new CashbackHackService();
+        int expected = 100;
+        int amount = 900;
+        int result = service.remain(amount);
+
+        assertEquals(result, expected);
+
+    }
+    @Test
+    public void testRemain3() {
+        CashbackHackService service = new CashbackHackService();
+        int expected = 800;
+        int amount = 1200;
+        int result = service.remain(amount);
+
+        assertEquals(result, expected);
+
+    }
+    @Test
+    public void testRemain4() {
+        CashbackHackService service = new CashbackHackService();
+        int expected = 2000;
+        int amount = 2000;
+        int result = service.remain(amount);
+
+        assertEquals(result, expected);
+
+    }
+    @Test
+    public void testRemain5() {
+        CashbackHackService service = new CashbackHackService();
+        int expected = 1;
+        int amount = 999;
+        int result = service.remain(amount);
+
+        assertEquals(result, expected);
+
+    }
 }
